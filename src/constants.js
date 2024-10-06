7f1bba7c2596b65be7e600d313cc85c0f848ad50
Mirth Pawar <mirth0708@gmail.com>
2024-10-06 18:39:58 +0530

    added deployed backend url

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,1 @@
-// export const BASE_URL = import.meta.env.VITE_BACKEND_URL || import.meta.env.BACKEND_URL;
-
-export const BASE_URL = "https://vid-tube-backend-2.onrender.com/api/v1"
+export const BASE_URL = import.meta.env.VITE_RENDER_URI || import.meta.env.VITE_LOCAL_URI;
